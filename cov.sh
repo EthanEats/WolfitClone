3f3cfe2791063ab74fdc3d6e0777ab3773dc2e0c
Ethan DeVries <edevries21@mail.wou.edu>
2025-04-17 23:22:05 -0700

    Initial commit.  Wolfit runs, coverage/testing works, populating database works.

--- cov.sh
+++ cov.sh
@@ -6,10 +6,3 @@ export FLASK_DEBUG=0
 coverage run --source "app/" --omit "app/commands.py" -m pytest
 coverage html
 open htmlcov/index.html
-
-set -e
-if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-        sensible-browser htmlcov/index.html
-else
-        open htmlcov/index.html
-fi
